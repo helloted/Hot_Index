3aa6d1848deaf6b617fa4b3a016349036eb0c16f
iMac <helloted@live.com>
2017-11-02 14:30:18 +0800

    Get

--- get.py
+++ get.py
@@ -1,19 +1,18 @@
 #encoding=utf-8
-from weibo_hot_model import WeiboHot,Session
+from hot_model import HotModel,Session
 
 
 def get_weibo_info():
     session = Session()
-    hot_models = session.query(WeiboHot).filter((WeiboHot.time-WeiboHot.init_time) > 12 * 60 * 60).all()
+    hot_models = session.query(HotModel).filter(HotModel.continued_time > 24 * 60 * 60).all()
     temp_dict = {}
     for hot in hot_models:
-        continued_time = hot.time - hot.init_time
         if hot.title in temp_dict:
             temp_time = temp_dict[hot.title]
-            if temp_time < continued_time:
+            if temp_time < hot.continued_time:
                 temp_dict[hot.title] = temp_time
         else:
-            temp_dict[hot.title] = continued_time
+            temp_dict[hot.title] = hot.continued_time
 
     for key in temp_dict:
         print key,temp_dict[key]
